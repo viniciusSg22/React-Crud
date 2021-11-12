29d0e6916a197e9d85f4def1c9ab9f72f5c556c7
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2021-11-12 18:53:30 -0300

    Conectando ao schema no banco de dados via sequelize

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const cors = require("cors");
-const mysql = require("mysql2");
-
 const app = express();
-app.listen(3001, () =>{
-    console.log("Servidor rodando na porta 3001")
-})
+
+const db = require("./models");
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("Servidor rodando na porta 3001");
+  });
+});
