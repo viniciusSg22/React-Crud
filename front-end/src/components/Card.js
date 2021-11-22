c6c9286372ef927ade60c2a1f6adb1f583fbe704
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2021-11-22 17:16:07 -0300

    Mudando componente card de Card para Blackquote

--- front-end/src/components/Card.js
+++ front-end/src/components/Card.js
@@ -2,16 +2,17 @@ import React from "react";
 
 const Card = (props) => {
   return (
-    <div className="card border-danger mb-3" style={{ maxWidth: "28rem" }}>
-      <h5 className="card-header bg-transparent border-danger card-title">
-        {props.title}
-      </h5>
+    <div className="card" style={{ maxWidth: "45rem"}}>
+      <h5 className="card-header card-title bg-primary text-white mb-3">{props.title}</h5>
       <div className="card-body text-dark">
-        <p className="card-text">{props.text}</p>
+        <blockquote className="blockquote mb-0">
+          <p>{props.text}</p>
+          <footer className="blockquote-footer">{props.footer}</footer>
+        </blockquote>
       </div>
-      <div className="card-footer bg-transparent border-danger">
+      {/* <div className="card-footer bg-transparent border-danger">
         {props.footer}
-      </div>
+      </div> */}
     </div>
   );
 };
