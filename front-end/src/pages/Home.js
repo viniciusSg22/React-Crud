74e85cddd9837d0afacc45821aedfa4dd7e60cb0
Vinícius Sanches Geniselli <vsgeniselli@gmail.com>
2021-11-29 14:45:41 -0300

    Implementando filtro de posts

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -5,16 +5,38 @@ import { useNavigate } from "react-router-dom";
 
 function Home() {
   const [listOfPosts, setListOfPosts] = useState([]);
+  const [busca, setBusca] = useState("");
+
   let navigate = useNavigate();
+
+  const filter = listOfPosts.filter((letters) => {
+    return Object.values(letters)
+      .join("")
+      .toLowerCase()
+      .includes(busca.toLowerCase());
+  });
+
   useEffect(() => {
     axios.get("http://localhost:3001/posts").then((response) => {
       setListOfPosts(response.data);
     });
-  }, []);
+  }, [busca]);
 
   return (
     <div className="App">
-      {listOfPosts.map((value, keyName) => {
+      <div>
+        <div className="col-sm-5 mx-auto mb-3 mt-3">
+          <input
+            onChange={(e) => {
+              setBusca(e.target.value);
+            }}
+            value={busca}
+            className="form-control"
+            placeholder="Filtro de Posts"
+          />
+        </div>
+      </div>
+      {filter.map((value, keyName) => {
         return (
           <div
             onClick={() => {
